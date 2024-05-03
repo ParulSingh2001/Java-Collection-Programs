6d8deed425a96f7ab2923bb69bf24bb93cbad972
Parul Singh <141449061+ParulSingh2001@users.noreply.github.com>
2024-05-03 10:13:34 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,22 @@
+package JavaCollection;
+
+import java.util.ArrayList;
+
+public class Add {
+    public static void main(String[] args){
+        ArrayList<String> str =new ArrayList<String>();
+        ArrayList<String> list =new ArrayList<String>();
+        list.add("Mango");
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Grapes");
+        System.out.println(list);
+        str.add("Scaler");
+        str.add("Topics");
+        str.add("Rocks");
+
+
+        System.out.println("ArrayList is" + str);
+    }
+
+}
